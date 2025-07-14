cbd7debd5359536ba5520fbab6e7e92ba9b97ea3
ivancarlosantos <ivansantos.ronem@gmail.com>
2025-07-14 19:07:09 -0300

    feat: first commit

---
+++ src/main/java/core/api/prime_max/PrimeMaxApplication.java
@@ -0,0 +1,13 @@
+package core.api.prime_max;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrimeMaxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrimeMaxApplication.class, args);
+	}
+
+}
---
+++ src/test/java/core/api/prime_max/PrimeMaxApplicationTests.java
@@ -0,0 +1,13 @@
+package core.api.prime_max;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrimeMaxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
