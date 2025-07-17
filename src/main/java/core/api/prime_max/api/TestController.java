28077142e06d911bddf5b15f0b2453eb7889e3fb
ivancarlosantos <ivansantos.ronem@gmail.com>
2025-07-17 13:02:21 -0300

    feat: add request controller test

---
+++ src/main/java/core/api/prime_max/api/TestController.java
@@ -0,0 +1,45 @@
+package core.api.prime_max.api;
+
+import lombok.*;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+import java.util.Date;
+import java.util.UUID;
+
+@Slf4j
+@RestController
+@RequestMapping(path = "/api/v2")
+public class TestController {
+
+    @GetMapping(path = "/address")
+    public ResponseEntity<TestFlow> test() throws UnknownHostException {
+        TestFlow response = TestFlow.builder()
+                .hash(UUID.randomUUID().toString())
+                .owner(InetAddress.getLocalHost().getHostName())
+                .address(InetAddress.getByName(InetAddress.getLocalHost().getHostAddress()))
+                .date(new Date().toString())
+                .build();
+        log.info("owner: {}  address: {}", response.getOwner(), response.getAddress());
+        return ResponseEntity.status(HttpStatus.OK).body(response);
+    }
+
+    @Getter
+    @Setter
+    @AllArgsConstructor
+    @NoArgsConstructor
+    @Builder
+    public static class TestFlow {
+
+        private String owner;
+        private InetAddress address;
+        private String hash;
+        private String date;
+    }
+}
