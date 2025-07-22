4223345b2900609af0f70afb194bc4fffa48bc05
ivancarlosantos <ivansantos.ronem@gmail.com>
2025-07-21 21:22:46 -0300

    feat: add conexão banco, swagger, ajustes na doc

--- src/main/java/core/api/prime_max/PrimeMaxApplication.java
+++ src/main/java/core/api/prime_max/PrimeMaxApplication.java
@@ -1,13 +1,16 @@
 package core.api.prime_max;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.http.HttpStatus;
 
+@Slf4j
 @SpringBootApplication
 public class PrimeMaxApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(PrimeMaxApplication.class, args);
+		log.info("Application UP {}", HttpStatus.OK);
 	}
-
 }
